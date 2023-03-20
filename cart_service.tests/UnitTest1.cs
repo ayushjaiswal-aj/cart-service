e375db26f0cf0ed2511767376270d5c9ba0d2c56
Ayush Jaiswal <ayushjaiswalaj@outlook.com>
2023-03-20 21:10:00 +0530

    Adds test environment to project

---
+++ cart_service.tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace cart_service.tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
